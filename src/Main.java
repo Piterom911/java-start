b2e8138f5e4c875e5d9d2e3c07ed9d36b6bed18d
Roman Dogoda <piterom911@gmail.com>
2024-09-24 19:16:50 +0200

    HW1 / Work with primitives

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello JAVA!  ");
 
-        String name = "Roman";
-        int age = 34;
+        char chG = 'G';
+        int num = 89;
+        byte smallNum = 4;
+        short shortNum = 56;
+        float floatNum = 4.7333436F;
+        double doubleNum = 4.355453532;
+        long longNum = 12121L;
 
-        System.out.printf("Hello, my name is %s. I'm %d years old.", name, age);
+        System.out.printf("Character: %c %n", chG);
+        System.out.printf("Integer: %d %n", num);
+        System.out.printf("Byte: %d %n", smallNum);
+        System.out.printf("Short: %d %n", shortNum);
+        System.out.printf("Float: %f %n", floatNum);
+        System.out.printf("Double: %f %n", doubleNum);
+        System.out.printf("Long: %d %n", longNum);
+
     }
 }
