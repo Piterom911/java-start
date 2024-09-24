53a57ef10d0cb04e9aaff0136a87f0831866ef30
Roman Dogoda <piterom911@gmail.com>
2024-09-24 18:55:30 +0200

    create all types of data

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello JAVA!  ");
+
+        String name = "Roman";
+        int age = 34;
+
+        System.out.printf("Hello, my name is %s. I'm %d years old.", name, age);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,24 @@
+public class Test {
+    public static void main(String[] args) {
+        boolean bool = true;
+        char character = 'a';
+        byte smallNum = 127;
+        short shortNum = 12343;
+        int num = 1234123412;
+        long bigNum = 1234123412341234L;
+        float floatNum = 1234.12234F;
+        double doubleNum = 1234.123412341213;
+
+        String str = "This is a usual string.";
+
+        System.out.println(bool);
+        System.out.println(character);
+        System.out.println(smallNum);
+        System.out.println(shortNum);
+        System.out.println(num);
+        System.out.println(bigNum);
+        System.out.println(floatNum);
+        System.out.println(doubleNum);
+        System.out.println(str);
+    }
+}
