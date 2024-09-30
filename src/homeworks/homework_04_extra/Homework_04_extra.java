380a50abb79669790292f526e4bc410cdfb854e6
Roman Dogoda <piterom911@gmail.com>
2024-09-30 16:49:23 +0200

    Homework 4 extra part / Say hello with name / (30.09.24)

---
+++ src/homeworks/homework_04_extra/Homework_04_extra.java
@@ -0,0 +1,23 @@
+package homeworks.homework_04_extra;
+
+import java.util.Scanner;
+
+public class Homework_04_extra {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.next();
+
+        char firstChar = name.charAt(0);
+        char lastChar = name.charAt(name.length() - 1);
+
+        System.out.printf("Good day, %s! \n", name);
+        System.out.printf("Your name starts with character <%c> and ends with character <%c>.", firstChar, lastChar);
+    }
+
+    public static void processString(String input) {
+
+    }
+}
+
