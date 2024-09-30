dad8c438156925adc961f0b8d6ddb83702a20caf
Roman Dogoda <piterom911@gmail.com>
2024-09-30 16:38:01 +0200

    Homework 4 part2 / calculator / (30.09.24)

---
+++ src/homeworks/homework_04_02/Calculator_04.java
@@ -0,0 +1,21 @@
+package homeworks.homework_04_02;
+
+public class Calculator_04 {
+
+    public static int sum (int a, int b) {
+        return a + b;
+    }
+
+    public static int sub (int a, int b) {
+        return a - b;
+    }
+
+    public static int mul (int a, int b) {
+        return a * b;
+    }
+
+    public static float div (int a, int b) {
+        return (float) a / b;
+    }
+
+}
---
+++ src/homeworks/homework_04_02/Calculator_04_main.java
@@ -0,0 +1,13 @@
+package homeworks.homework_04_02;
+
+public class Calculator_04_main {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 20;
+
+        System.out.println("Sum result: " + Calculator_04.sum(a, b));
+        System.out.println("Subtraction result: " + Calculator_04.sub(a, b));
+        System.out.println("Multiplication result: " + Calculator_04.mul(a, b));
+        System.out.println("Division result: " + Calculator_04.div(a, b));
+    }
+}
