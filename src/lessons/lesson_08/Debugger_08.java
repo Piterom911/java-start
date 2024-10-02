bc62e3894cf20023a446bcadfa1df095438d0ab3
Roman Dogoda <piterom911@gmail.com>
2024-10-02 09:14:25 +0200

    lesson 8 / debugger / (02.10.24)

---
+++ src/lessons/lesson_08/Debugger_08.java
@@ -0,0 +1,15 @@
+package lessons.lesson_08;
+
+public class Debugger_08 {
+    public static void findEvenOdd(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num+" is even"); }
+        else {
+            System.out.println(num+" is odd"); }
+    }
+    public static void main (String[] args) {
+        findEvenOdd(2);
+        findEvenOdd(4);
+        findEvenOdd(5);
+    }
+}
